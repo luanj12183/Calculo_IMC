cccd1c9c5ae724fd1a8a385e3a797cefcaf524ed
LUAN JUSTINIANO DOS SANTOS <luanj12183@gmail.com>
2026-06-08 18:25:14 +0000

    ...

--- Models/ImcViewModel.cs
+++ Models/ImcViewModel.cs
@@ -1,15 +1,15 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace CalculadoraImc.Models
+namespace Calculo_IMC.Models 
 {
-    public class ImcViewModel
+    public class ImcViewModel 
     {
         [Required(ErrorMessage = "O peso é obrigatório.")]
         [Range(10, 300, ErrorMessage = "Digite um peso válido (entre 10kg e 300kg).")]
-        public double Peso { get; set ; }
+        public double Peso { get; set; }
 
         [Required(ErrorMessage = "A altura é obrigatória.")]
-        [Range(0.5, 2.5, ErrorMessage ="Digite uma altura válida (entre 0.5m e 2.5m).")]
+        [Range(0.5, 2.5, ErrorMessage = "Digite uma altura válida (entre 0.5m e 2.5m).")]
         public double Altura { get; set; }
 
         public double ResultadoImc { get; set; }
